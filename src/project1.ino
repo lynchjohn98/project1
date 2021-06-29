c0f810f64b64ce96b649e4f669a01002ed4f61f3
Your Name <lynchjohn98@gmail.com>
2021-06-29 11:20:54 -0500

    Updating items with a comment

--- src/project1.ino
+++ src/project1.ino
@@ -8,13 +8,13 @@
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+  //this is a test
 }
 
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  
+
 
 }
